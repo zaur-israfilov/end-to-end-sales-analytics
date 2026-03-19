21369a21fd055d1dee88ff8c5af1cab00be1dea7
Zaur İsrafilov <israfilovzaur1@gmail.com>
2026-03-19 13:47:53 +0400

    Update schema.sql

--- sql/schema.sql
+++ sql/schema.sql
@@ -1,9 +1,18 @@
 CREATE TABLE sales (
-    total_bill FLOAT,
-    tip FLOAT,
-    sex VARCHAR(10),
-    smoker VARCHAR(10),
-    day VARCHAR(10),
-    time VARCHAR(10),
-    size INT
+    row_id INT,
+    order_id VARCHAR(50),
+    order_date DATE,
+    ship_date DATE,
+    customer_name VARCHAR(100),
+    segment VARCHAR(50),
+    country VARCHAR(50),
+    city VARCHAR(50),
+    state VARCHAR(50),
+    product_name VARCHAR(150),
+    category VARCHAR(50),
+    sub_category VARCHAR(50),
+    sales FLOAT,
+    quantity INT,
+    discount FLOAT,
+    profit FLOAT
 );
